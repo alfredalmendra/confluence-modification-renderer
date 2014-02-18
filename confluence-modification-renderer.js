c8c886465abad686779ffb8314aae04f34caa19b
Alfred Almendra <alfred.almendra@gmail.com>
2014-02-18 23:21:04 +0100

    Simple hide/show rendering on div

---
+++ confluence-modification-renderer.js
@@ -0,0 +1,14 @@
+function renderVersion(renderedVersion) {
+  $('div').each(function(index, element) {
+    var modifAction = $(this).attr('modificationAction');
+    var modifVersion = $(this).attr('modificationVersion');
+    if( (modifAction == 'add' && modifVersion > renderedVersion)
+        || (modifAction == 'delete' && modifVersion <= renderedVersion) ) {
+      $(this).attr('renderStyle', 'hidden');
+      $(this).attr('hidden', true);
+    } else if(modifAction !== undefined && modifVersion !== undefined) {
+      $(this).attr('renderStyle', 'visible');
+      $(this).attr('hidden', false);
+    }
+  });
+}
---
+++ tests.js
@@ -0,0 +1,48 @@
+var div;
+module( "Div rendering", {
+  setup: function() {
+    div = document.createElement('div');
+    document.body.appendChild(div);
+  },
+  teardown: function() {
+    div.remove();
+  }
+});
+function setAttributes(modificationAction, modificationVersion) {
+  div.setAttribute('modificationAction', modificationAction);
+  div.setAttribute('modificationVersion', modificationVersion);
+}
+function verifyRendering(hidden, renderStyle) {
+  ok( div.hidden == hidden, "Passed!" );
+  ok( div.getAttribute('renderStyle') == renderStyle, "Passed!" );
+}
+test( "Div with no specific attributes remains unchanged", function() {
+  renderVersion(1);
+  ok( div.hidden == false, "Passed!" );
+  ok( div.hasAttribute('renderStyle') == false, "Passed!" );
+});
+test( "Div added in version 2 is hidden while reading version 1", function() {
+  setAttributes('add', 2);
+  renderVersion(1);
+  verifyRendering(true, "hidden");
+});
+test( "Div deleted in version 1 is hidden while reading version 2", function() {
+  setAttributes('delete', 1);
+  renderVersion(2);
+  verifyRendering(true, "hidden");
+});
+test( "Div deleted in version 1 is hidden while reading version 1", function() {
+  setAttributes('delete', 1);
+  renderVersion(1);
+  verifyRendering(true, "hidden");
+});
+test( "Div deleted in version 2 is visible while reading version 1", function() {
+  setAttributes('delete', 2);
+  renderVersion(1);
+  verifyRendering(false, "visible");
+});
+test( "Div added in version 2 is visible while reading version 2", function() {
+  setAttributes('add', 2);
+  renderVersion(2);
+  verifyRendering(false, "visible");
+});
